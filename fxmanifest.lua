068e93b6000a61342589b39248dd8e83f89e26ac
Bizarre <89548876+bizarredevs@users.noreply.github.com>
2024-10-12 16:11:56 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bizarre'
+description 'Basic Discord Role Checker for FiveM.'
+version '1.0.0'
+
+client_script 'role_checker.lua' -- Name of your main script file
